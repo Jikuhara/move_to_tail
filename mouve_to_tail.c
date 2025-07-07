ffba065409f670d77688a7ffa059e09bae508bd5
Jikuhara <kei2003730@gmail.com>
2025-07-07 14:16:57 +0900

    modified:   a.out 	new file:   memo.md 	modified:   mouve_to_tail.c

--- mouve_to_tail.c
+++ mouve_to_tail.c
@@ -6,11 +6,12 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 14:37:08 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/06 14:56:08 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/06 16:36:31 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
 void swap(int *a, int *b)
 {
@@ -45,14 +46,28 @@ int move_zero_to_tail(int nums[], int size)
     return (count);
 }
 
-int main(void)
+void test_move_to_zero_tail(int nums[], int size)
 {
-    int nums[] = {0, 3, 1, 0};
-    int size = 4;
-    move_zero_to_tail(nums, size);
+    int rtv = move_zero_to_tail(nums, size);
     for (int i = 0; i < size; i++)
     {
         printf("nums[%i] = %i\n", i, nums[i]);
     }
+    printf("Return value = %i\n", rtv);
+}
+
+int main(void)
+{
+    test_move_to_zero_tail((int[]){0, 3, 1, 0}, 4);
+    test_move_to_zero_tail((int[]){0}, 1);
+    // test_move_to_zero_tail((int[]){}, 0);
+    test_move_to_zero_tail((int[]){1, 0}, 2);
+    int nums_10k[10000];
+    memset(nums_10k, 0, sizeof(int) * 10000);
+    test_move_to_zero_tail(nums_10k, 10000);
+    int size = 100000;
+    int nums_size[size];
+    memset(nums_size, 0, sizeof(int) * size);
+    test_move_to_zero_tail(nums_size, size);
     return (0);
 }
